ea77f7ab72d7d457bba9e274b9ded4db2ded5bd8
Paul-Emmanuel Garcia <garcia.paulemmanuel@gmail.com>
2016-12-11 16:27:53 +0100

    Initial Commit

---
+++ ImageLoader/ImageLoader.h
@@ -0,0 +1,19 @@
+//
+//  ImageLoader.h
+//  ImageLoader
+//
+//  Created by Paul-Emmanuel on 11/12/16.
+//  Copyright © 2016 rstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageLoader.
+FOUNDATION_EXPORT double ImageLoaderVersionNumber;
+
+//! Project version string for ImageLoader.
+FOUNDATION_EXPORT const unsigned char ImageLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageLoader/PublicHeader.h>
+
+
